4d86bd4935cc1e22b1ebb6a77bf71d2d7ad13766
prasanna sundar <sundar@MacBook-Pro.local>
2021-12-06 16:13:34 -0500

    make it better

---
+++ src/components/Bookbox.vue
@@ -0,0 +1,42 @@
+<template>
+    
+     <div class="bookbox">
+         
+        <div v-for="book in books" :key="book.id" >
+                
+                <Book :book="book"/>
+        </div>
+
+        
+
+     </div>
+
+</template>
+
+<script>
+
+import Book from "./Book.vue"
+export default{
+
+    name: 'Bookbox',
+    components: {
+        Book
+    },
+    props: {
+        books: Object
+    }
+
+}
+
+</script>
+
+
+<style>
+
+.bookbox{
+
+    background-color: bisque;
+    padding: 1em;
+    color: gray;
+}
+</style>
